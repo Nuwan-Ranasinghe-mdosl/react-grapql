caf002a0b87ef5de836a8f360dab1ee6048d596f
Nuwan <nwnhemantha@gmail.com>
2022-02-24 16:00:53 +0530

    feat: Gotchas with queries in react

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
-const SongList = (props) => {
+class SongList extends Component {
 
-  console.log(props);
-  return (
-    <div>SongList</div>
-  )
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  };
+
+  render() {
+    if (this.props.data.loading) {
+      return <div>Loading ...</div>
+    }
+
+    return (
+      <ul className='collection'>{this.renderSongs()}</ul>
+    )
+  }
 }
 
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
