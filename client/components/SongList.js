fe7a9a264c317bc3d8b5b06d0211c8c1d52aa5b7
Nuwan <nwnhemantha@gmail.com>
2022-02-24 23:24:31 +0530

    feat: Automatic data caching

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -7,7 +7,10 @@ import query from '../queries/fetchSongs';
 class SongList extends Component {
 
   onSongDelete(id) {
-    this.props.mutate({ variables: { id } });
+    this.props.mutate({
+      variables: { id }
+    })
+      .then(() => this.props.data.refetch());
   }
 
   renderSongs() {
